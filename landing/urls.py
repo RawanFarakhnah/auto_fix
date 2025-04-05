461c54289e7f49f6c2e7be6696a0d5f0dcc75cbe
yasmeenayousef <yasmeenabassam@gmail.com>
2025-04-05 22:51:02 +0300

    design dashboard

--- landing/urls.py
+++ landing/urls.py
@@ -5,4 +5,8 @@ app_name = 'landing'
 
 urlpatterns = [
     path('', views.root, name='root'),
+    path('dashboard',views.dashboard,name='dashboard'),
+    path('user',views.user,name='user'),
+    path('add',views.add_user,name='add_user'),
+    
 ]
--- landing/views.py
+++ landing/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def root(request):
     return render(request, 'landing.html')
+def dashboard(request):
+    return render(request,'dashboard.html')
+def user(request):
+    return render(request,'user/user.html')
+def add_user(request):
+    return render(request,'user/add_user.html')
+ 
