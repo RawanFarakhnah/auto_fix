f4d6170eb9396030deaa71ba2372ab7128fdc303
yasmeenayousef <yasmeenabassam@gmail.com>
2025-04-06 18:43:33 +0300

    complate design

--- landing/urls.py
+++ landing/urls.py
@@ -8,5 +8,14 @@ urlpatterns = [
     path('dashboard',views.dashboard,name='dashboard'),
     path('user',views.user,name='user'),
     path('add',views.add_user,name='add_user'),
+    path('update/<int:user_id>',views.update_user,name='update_user'),
+    path('booking/add',views. add_book,name='add_book'),
+    path('booking',views.update_booking,name='update_booking'),
+    path('workshop',views.list_workshop,name='workshop'),
+    path('workshop/add',views.add_workshop,name='add_workshop'),
+    path('review/add',views.add_review,name='add_review'),
+    path('review/list',views.list_review,name='add_list'),
+    path('review/adminr',views.admin_review,name='admin_review')
+
     
 ]
--- landing/views.py
+++ landing/views.py
@@ -9,4 +9,22 @@ def user(request):
     return render(request,'user/user.html')
 def add_user(request):
     return render(request,'user/add_user.html')
+def update_user(request,user_id):
+    return render(request,'user/update_user.html')
+def update_booking(request):
+    return render(request,'booking/add_booking.html')
+def add_book(request):
+    return render(request,'booking/booking.html')
+def add_workshop(request):
+    return render(request,'workshop/add_workshop.html')
+def list_workshop(request):
+    return render(request,'workshop/workshop.html')
+def add_review(request):
+    return render(request,'review/add_review.html')
+def list_review(request):
+    return render(request,'review/list_review.html')
+def admin_review(request):
+    return render(request,'review/admin_review.html')
+
+
  
